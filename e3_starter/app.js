31304e4009c58806146d046450493fc706b325d1
root <root@DESKTOP-NAPHJE9.localdomain>
2018-11-18 01:22:05 -0500

    backup

--- e3_starter/app.js
+++ e3_starter/app.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const log = console.log
+const datetime = require('date-and-time');
 const yargs = require('yargs').option('addRest', {
     type: 'array' // Allows you to have an array of arguments for particular command
   }).option('addResv', {
@@ -13,15 +14,17 @@ const yargs = require('yargs').option('addRest', {
 const reservations = require('./reservations');
 
 const yargs_argv = yargs.argv
-//log(yargs_argv) // uncomment to see what is in the argument array
+log(yargs_argv) // uncomment to see what is in the argument array
 
 if ('addRest' in yargs_argv) {
 	const args = yargs_argv['addRest']
 	const rest = reservations.addRestaurant(args[0], args[1]);	
 	if (rest.length > 0) {
 		/* complete */ 
+		log("Added restaurant "+args[0]+".");
 	} else {
 		/* complete */ 
+		log("Duplicate restaurant not added.");
 	}
 }
 
@@ -30,34 +33,74 @@ if ('addResv' in yargs_argv) {
 	const resv = reservations.addReservation(args[0], args[1], args[2]);
 
 	// Produce output below
-	
+	log("Added reservation at "+args[0]+" on "+ datetime.format(resv.time, 'MMMM DD YYYY [at] hh:mm A')+ " for "+args[2]+" people.");
 }
 
 if ('allRest' in yargs_argv) {
 	const restaurants = reservations.getAllRestaurants(); // get the array
 	
 	// Produce output below
+	for(let i = 0; i < restaurants.length; i++) {
+		log(restaurants[i].name+": "+restaurants[i].description+" - "+restaurants[i].numReservations+" active reservations");
+	}
 }
 
 if ('restInfo' in yargs_argv) {
 	const restaurants = reservations.getRestaurtantByName(yargs_argv['restInfo']);
 
 	// Produce output below
-
+	log(restaurants.name+": "+restaurants.description+" - "+restaurants.numReservations+" active reservations");
 }
 
 if ('allResv' in yargs_argv) {
 	const restaurantName = yargs_argv['allResv']
-	const reservationsForRestaurant = reservations.getAllReservationsForRestaurant(restaurantName); // get the arary
+	let reservationsForRestaurant = reservations.getAllReservationsForRestaurant(restaurantName); // get the arary
 	
 	// Produce output below
+
+	log("Reservations for "+reservationsForRestaurant[0].restaurant+":");
+	reservationsForRestaurant.sort(function(a, b) {
+        let atime = new Date();
+		let btime = new Date();
+		atime = datetime.parse(a.time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		btime = datetime.parse(b.time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		if (datetime.subtract(atime, btime).toSeconds()>0){
+			return 1
+		}
+		else{
+			return -1
+		}
+	});
+	for(let i = 0; i < reservationsForRestaurant.length; i++) {
+		let curDate = new Date();
+		curDate = datetime.parse(reservationsForRestaurant[i].time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		log("- "+datetime.format(curDate, 'MMM. DD YYYY, h:mm A')+", table for "+reservationsForRestaurant[i].people);
+	}
 }
 
 if ('hourResv' in yargs_argv) {
 	const time = yargs_argv['hourResv']
-	const reservationsForRestaurant = reservations.getReservationsForHour(time); // get the arary
+	let all_reservations = reservations.getReservationsForHour(time); // get the arary
 	
 	// Produce output below
+	log("Reservations in the next hour:");
+	all_reservations.sort(function(a, b) {
+        let atime = new Date();
+		let btime = new Date();
+		atime = datetime.parse(a.time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		btime = datetime.parse(b.time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		if (datetime.subtract(atime, btime).toSeconds()>0){
+			return 1
+		}
+		else{
+			return -1
+		}
+	});
+	for(let i = 0; i < all_reservations.length; i++) {
+		let curDate = new Date();
+		curDate = datetime.parse(all_reservations[i].time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		log(all_reservations[i].restaurant+": "+datetime.format(curDate, 'MMM[.] DD YYYY[,] h:mm A')+", table for "+all_reservations[i].people);
+	}
 }
 
 if ('checkOff' in yargs_argv) {
@@ -65,6 +108,9 @@ if ('checkOff' in yargs_argv) {
 	const earliestReservation = reservations.checkOffEarlistReservation(restaurantName); 
 	
 	// Produce output below
+	let curDate = new Date();
+	curDate = datetime.parse(earliestReservation.time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+	log("Checked off reservation on "+datetime.format(curDate, 'MMM[.] DD YYYY[,] h:mm A')+", table for "+earliestReservation.people);
 }
 
 if ('addDelay' in yargs_argv) {
@@ -72,12 +118,33 @@ if ('addDelay' in yargs_argv) {
 	const resv = reservations.addDelayToReservations(args[0], args[1]);	
 
 	// Produce output below
-	
+	log("Reservations for "+resv[0].restaurant+":");
+	resv.sort(function(a, b) {
+        let atime = new Date();
+		let btime = new Date();
+		atime = datetime.parse(a.time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		btime = datetime.parse(b.time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		if (datetime.subtract(atime, btime).toSeconds()>0){
+			return 1
+		}
+		else{
+			return -1
+		}
+	});
+	for(let i = 0; i < resv.length; i++) {
+		let curDate = new Date();
+		curDate = datetime.parse(resv[i].time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		log("- "+datetime.format(curDate, 'MMM[.] DD YYYY')+", table for "+resv[i].people);
+	}
 }
 
 if ('status' in yargs_argv) {
 	const status = reservations.getSystemStatus()
 
 	// Produce output below
+    log("Number of restaurants: "+status.numRestaurants.toString());
+    log("Number of total reservations: "+status.totalReservations.toString());
+    log("Busiest restaurant: "+status.currentBusiestRestaurantName);
+    log("System started at: "+datetime.format(status.systemStartTime, 'MMM[.] DD YYYY[,] h:mm A'));
 }
 
--- e3_starter/reservations.js
+++ e3_starter/reservations.js
@@ -34,21 +34,48 @@ const getSystemStatus = () => {
 
 /* Helper functions to save JSON */
 const updateSystemStatus = () => {
-	const status = {}
+	let status = getSystemStatus();
 	
 	/* Add your code below */
+	const allRestaurants = getAllRestaurants();
+	const allReservations = getAllReservations();
+	let curBusiestRestaurantName = '';
+	let maxResCount = 0;
+	let ResCount_i = 0;
+	for (let i = 0; i < allRestaurants.length; i++) {
+		ResCount_i = allRestaurants[i].numReservations;
+		if (ResCount_i > maxResCount){
+			maxResCount = ResCount_i;
+			curBusiestRestaurantName = allRestaurants[i].name;
+		}
+	}
+	status.numRestaurants = allRestaurants.length;
+	status.totalReservations = allReservations.length;
+	status.currentBusiestRestaurantName = curBusiestRestaurantName;
 
 	fs.writeFileSync('status.json', JSON.stringify(status))
 }
 
 const saveRestaurantsToJSONFile = (restaurants) => {
 	/* Add your code below */
+	
+	if (restaurants.length > 0){
+		fs.writeFileSync('restaurants.json', JSON.stringify(restaurants))
+	}
+	else{
+		fs.writeFileSync('restaurants.json', JSON.stringify([]))
+	}
 
 };
 
 const saveReservationsToJSONFile = (reservations) => {
 	/* Add your code below */
-
+	if (reservations.length > 0){
+		fs.writeFileSync('reservations.json', JSON.stringify(reservations))
+	}
+	else{
+		fs.writeFileSync('reservations.json', JSON.stringify([]))
+	}
 };
 
 /*********/
@@ -56,13 +83,27 @@ const saveReservationsToJSONFile = (reservations) => {
 // Should return an array
 const addRestaurant = (name, description) => {
 	// Check for duplicate names
-
-	return [];
+    let allRestaurants = getAllRestaurants();
+    let duplicate_name_found = 0;
+	for (let i = 0; i < allRestaurants.length; i++) {
+		if (allRestaurants[i].name == name){
+			duplicate_name_found = 1;
+			break;
+		}
+	}
+	if (duplicate_name_found == 1){
+		return [];
+	}
+	
 
 	// if no duplicate names:
-	const restaurant = null;
-
-
+	const restaurant = {
+		name: name,
+		description: description,
+		numReservations: 0,
+	};
+	allRestaurants.push(restaurant);
+    saveRestaurantsToJSONFile(allRestaurants);
 	return [restaurant];
 
 }
@@ -70,7 +111,21 @@ const addRestaurant = (name, description) => {
 const addReservation = (restaurant, time, people) => {
 	
 	/* Add your code below */
-	const reservation = null;
+	let currentAllReservations = getAllReservations();
+	const reservation = {
+		restaurant: restaurant,
+		time: datetime.parse(time, 'MMM D YYYY HH:mm:ss'),
+		people: people,
+	};
+	currentAllReservations.push(reservation);
+    saveReservationsToJSONFile(currentAllReservations);
+	let allRestaurants = getAllRestaurants();
+	for (let i = 0; i < allRestaurants.length; i++) {
+		if (allRestaurants[i].name == restaurant){
+			allRestaurants[i].numReservations++;
+		}
+	}
+    saveRestaurantsToJSONFile(allRestaurants);
 	return reservation;
 
 }
@@ -81,40 +136,128 @@ const addReservation = (restaurant, time, people) => {
 // Should return an array - check to make sure restaurants.json exists
 const getAllRestaurants = () => {
 	/* Add your code below */
+	if (fs.existsSync('restaurants.json')) {
+		const restaurants = fs.readFileSync('restaurants.json');
+		return JSON.parse(restaurants);
+	}
+	else{
+		return [];
+	}
+
 };
 
 
 const getRestaurtantByName = (name) => {
 	/* Add your code below */
+	const allRestaurants = getAllRestaurants();
+	for (let i = 0; i < allRestaurants.length; i++) {
+		if (allRestaurants[i].name == name){
+			return allRestaurants[i]
+		}
+	}
 };
 
 // Should return an array - check to make sure reservations.json exists
 const getAllReservations = () => {
-  /* Add your code below */
+    /* Add your code below */
+	if (fs.existsSync('reservations.json')) {
+		const reservations = fs.readFileSync('reservations.json');
+		return JSON.parse(reservations);
+	}
+	else{
+		return [];
+	}
 };
 
 // Should return an array
 const getAllReservationsForRestaurant = (name) => {
 	/* Add your code below */
+	let targetReservations = [];
+	const allReservations = getAllReservations();
+	for (let i = 0; i < allReservations.length; i++) {
+		if (allReservations[i].restaurant == name){
+			targetReservations.push(allReservations[i]);
+		}
+	}
+
+	return targetReservations;
 };
 
 
 // Should return an array
 const getReservationsForHour = (time) => {
 	/* Add your code below */
+	let parsedTime = datetime.parse(time, 'MMM D YYYY HH:mm:ss'); 
+	let targetReservations = [];
+	const nextHour = datetime.addHours(parsedTime, 1);
+	const allReservations = getAllReservations();
+	for (let i = 0; i < allReservations.length; i++) {
+        let allReservationsItime = new Date();
+		allReservationsItime = datetime.parse(allReservations[i].time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		if (datetime.subtract(allReservationsItime, parsedTime).toSeconds() > 0 ){
+			if (datetime.subtract(nextHour, allReservationsItime).toSeconds() > 0 ){
+				targetReservations.push(allReservations[i]);
+			}			
+		}
+	}
+	return targetReservations;
+
 }
 
 
 const checkOffEarliestReservation = (restaurantName) => {
 	
-	const checkedOffReservation = null;
+	let checkedOffReservation = null;
+	const allReservations4Restaurant = getAllReservationsForRestaurant(restaurantName);
+
+	let latestTime = new Date();
+	latestTime = datetime.addYears(latestTime,9999);
+	for (let i = 0; i < allReservations4Restaurant.length; i++) {
+        let allReservations4RestaurantItime = new Date();
+		allReservations4RestaurantItime = datetime.parse(allReservations4Restaurant[i].time, "YYYY-MM-DD HH:mm:ss.SSSZ", true);
+		if (datetime.subtract(latestTime, allReservations4RestaurantItime).toSeconds() > 0){
+			checkedOffReservation = allReservations4Restaurant[i];
+			latestTime = allReservations4RestaurantItime;						
+		}
+	}
+    let index_to_remove = null;
+    let allReservations = getAllReservations();
+
+	for (let i = 0; i < allReservations.length; i++) {
+		if (allReservations[i].restaurant == checkedOffReservation.restaurant){
+			if(allReservations[i].time == checkedOffReservation.time){
+				if(allReservations[i].people == checkedOffReservation.people){
+					index_to_remove = i;
+					break;
+				}
+			}	
+		}
+	}
+    allReservations.splice(index_to_remove, 1);
+    saveReservationsToJSONFile(allReservations);
+
+	let allRestaurants = getAllRestaurants();
+	for (let i = 0; i < allRestaurants.length; i++) {
+		if (allRestaurants[i].name == restaurantName){
+			allRestaurants[i].numReservations--;
+		}
+	}
+    saveRestaurantsToJSONFile(allRestaurants);
  	return checkedOffReservation;
 }
 
 
 const addDelayToReservations = (restaurant, minutes) => {
 	// Hint: try to use array.map()
-	
+	let allReservations = getAllReservations();
+	for (let i = 0; i < allReservations.length; i++) {
+		if (allReservations[i].restaurant == restaurant){
+			allReservations[i].time = datetime.addMinutes(allReservations[i].time,minutes);
+		}
+	}
+    saveReservationsToJSONFile(allReservations);
+ 	return allReservations;
+
 }
 
 startSystem(); // start the system to create status.json (should not be called in app.js)
